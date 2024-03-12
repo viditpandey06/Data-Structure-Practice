f3c469e71d55fa572a34a73da457330fe8c1914d
[Vidit Pandey] <viditpandey06@gmail.com>
2024-03-13 00:01:18 +0530

    Create maximumSubArray.java

---
+++ maximumSubArray.java
@@ -0,0 +1,19 @@
+public class maximumSubArray {
+    public static void main(String[] args) {
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        System.out.println("The maximum sum of the subarray is: " + maxSubArray(nums));
+    }
+    
+    public static int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE, currSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currSum = 0;
+            for (int j = i; j < nums.length; j++) {
+                currSum = currSum + nums[j];
+            }
+            max = Math.max(currSum, max);
+        }
+
+        return max;
+    }
+}
