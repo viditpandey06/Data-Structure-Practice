d503fb6d25ab2e59449fe59fc116185da743a93c
[Vidit Pandey] <viditpandey06@gmail.com>
2024-03-12 18:46:00 +0530

    commit

---
+++ numberOfPaths.java
@@ -0,0 +1,10 @@
+public class numberOfPaths {
+    public static void main(String[] args) {
+        int m = 3, n = 3;
+        int paths = uniquePaths(m, n);
+        System.out.println("The number of unique paths are: " + paths);
+    }
+    public static int uniquePaths(int m, int n) {
+        
+    }
+}
