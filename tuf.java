616e78e42f8bfd53b88bef760259023925c47bbc
[Vidit Pandey] <viditpandey06@gmail.com>
2024-03-13 12:17:20 +0530

    Create tuf.java

---
+++ tuf.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+public class tuf {
+    public static int maxSubarraySum(int[] arr, int n) {
+        int maxi = Integer.MIN_VALUE; // maximum sum
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                // current subarray = arr[i.....j]
+
+                // add the current element arr[j]
+                // to the sum i.e. sum of arr[i...j-1]
+                sum += arr[j];
+
+                maxi = Math.max(maxi, sum); // getting the maximum
+            }
+        }
+
+        return maxi;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int n = arr.length;
+        int maxSum = maxSubarraySum(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
+
+    }
+
+}
