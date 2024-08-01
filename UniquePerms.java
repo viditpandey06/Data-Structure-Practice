dbf39c01655204842b3d643e69c08da031d29bdf
Vidit Pandey <viditpandey06@gmail.com>
2024-08-02 00:13:54 +0530

    Create UniquePerms.java

---
+++ UniquePerms.java
@@ -0,0 +1,7 @@
+public class UniquePerms{
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+        System.out.println(permute(nums));
+    }
+    Public static List<List<Integer>> permute()
+}
