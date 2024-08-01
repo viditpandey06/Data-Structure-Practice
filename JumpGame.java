b17be35df53726d741cc684db8350fda1c0f27ab
Vidit Pandey <viditpandey06@gmail.com>
2024-08-02 00:13:52 +0530

    Create JumpGame.java

---
+++ JumpGame.java
@@ -0,0 +1,18 @@
+
+public class JumpGame {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 0};
+        System.out.println(canJump(nums));
+    }
+
+    public static boolean canJump(int[] nums) {
+       boolean res=false;
+       
+
+
+       return res;
+    }
+}
+
+
