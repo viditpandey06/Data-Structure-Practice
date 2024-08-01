14d18c51cf956fe4e4a45255b184938350d944df
Vidit Pandey <viditpandey06@gmail.com>
2024-08-02 00:13:59 +0530

    Update WordSearch.java

--- WordSearch.java
+++ WordSearch.java
@@ -1,20 +1,20 @@
 public class WordSearch{
     public static void main(String[] args) {
        
-        char[][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
-        System.out.println(checkWord(board, "SEE"));
+        char[][] board = {{'A','B','C'},{'S','C','B'},{'E','F','G'}};
+        System.out.println(checkWord(board, "ABAFG"));
     }
     public static boolean checkWord(char[][] board, String word) {
         boolean res=false;
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[0].length; j++){
-                res= helper(board, word, i, j, 0, res);
-                if(res){
-                    return res;
-                }
-            }
-        }
-       //res= helper(board, word, 0, 0, 0, res);
+        // for(int i=0; i<board.length; i++){
+        //     for(int j=0; j<board[0].length; j++){
+        //         res= helper(board, word, i, j, 0, res);
+        //         if(res){
+        //             return res;
+        //         }
+        //     }
+        // }
+       res= helper(board, word, 0, 0, 0, res);
         return res;
     }
     public static boolean helper(char[][] board, String word, int i, int j, int index, boolean res){
