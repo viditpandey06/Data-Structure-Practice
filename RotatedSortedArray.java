015490212e306ab90727b2bee36567556b4fde63
Vidit Pandey <viditpandey06@gmail.com>
2024-09-14 23:38:30 +0530

    Update RotatedSortedArray.java

--- RotatedSortedArray.java
+++ RotatedSortedArray.java
@@ -1,6 +1,6 @@
 public class RotatedSortedArray {
      public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int[] arr = {4, 5, 6, 7,7, 0, 1, 2};
         int target = 0;
         System.out.println(search(arr, target));
      }
