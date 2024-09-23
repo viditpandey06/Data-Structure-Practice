a49a23cf87dd65a736ab0b61d39b008e5c006563
Vidit Pandey <viditpandey06@gmail.com>
2024-09-23 22:55:11 +0530

    Create StartingPointCycleLL.java

---
+++ StartingPointCycleLL.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        //detecting cycle
+        ListNode slow= head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                slow=head;
+                //distance from head to starting point of cycle is equal to distance from point of collision to start point of cycle.
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
