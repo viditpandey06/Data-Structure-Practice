054000b315c1d1b679c7a267e6abddcbd57cb381
Vidit Pandey <viditpandey06@gmail.com>
2024-10-14 09:56:26 +0530

    Create BinarySubarraysWithSum.java

---
+++ BinarySubarraysWithSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numSubarraysWithSum(int[] arr, int target) {
+       int sum=0;
+         int count = 0;
+         HashMap<Integer,Integer> map = new HashMap<>();
+         map.put(0,1);
+         for(int i=0;i<arr.length;i++) {
+             sum+=arr[i];
+             if(map.containsKey(sum-target)) {
+                 count+=map.get(sum-target);
+             }
+             map.put(sum,map.getOrDefault(sum,0)+1);
+         }
+         return count;
+    }
+}
